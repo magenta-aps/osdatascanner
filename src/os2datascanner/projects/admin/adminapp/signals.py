c5641469ad9086b07004ffeca3d8a67a07b659e9
Mikkel Rostved <mr@magenta-aps.dk>
2020-11-18 14:15:06 +0100

    [#39456] Try/catch when publishing

--- src/os2datascanner/projects/admin/adminapp/signals.py
+++ src/os2datascanner/projects/admin/adminapp/signals.py
@@ -4,6 +4,10 @@ from django.forms.models import model_to_dict
 from inspect import ismethod
 from os2datascanner.utils import amqp_connection_manager
 import json
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
 
 class Event():
     def __init__(self, event_type, model_class, instance, meta = {}):
@@ -16,22 +20,27 @@ class Event():
         return {
             "type": self.event_type,
             "model_class": self.instance.__class__.__name__,
-            "instance": self.instance.to_json_object() 
+            "instance": self.instance.to_json_object_bah() 
                             if hasattr(self.instance, "to_json_object") and 
                                ismethod(getattr(self.instance, "to_json_object"))
                             else "missing to_json_object() method"
         }
 
 def publish_events(events):
-    # TODO: use settings.AMQP_EVENTS_TARGET or similar
-    queue = "os2ds_model_change_events"
-    amqp_connection_manager.start_amqp(queue)
-    for event in events:
-        json_event = event.to_json_object()
-        print("published: {0}".format(json_event))
-        amqp_connection_manager.send_message(
-            queue, json.dumps(json_event))
-    amqp_connection_manager.close_connection()
+    try:
+        # TODO: use settings.AMQP_EVENTS_TARGET or similar
+        queue = "os2ds_model_change_events"
+        amqp_connection_manager.start_amqp(queue)
+        for event in events:
+            json_event = event.to_json_object()
+            print("published: {0}".format(json_event))
+            amqp_connection_manager.send_message(
+                queue, json.dumps(json_event))
+        amqp_connection_manager.close_connection()
+    except Exception as e: 
+        # log the error
+        logger.error("Could not publish event. Error: "+format(e))
+
 
 @receiver(post_save)
 def post_save_callback(**kwargs):
