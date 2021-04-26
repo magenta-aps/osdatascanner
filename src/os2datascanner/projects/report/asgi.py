681e75744c742da7e949e1c0099c108984a67a61
Mikkel Rostved <mr@magenta-aps.dk>
2021-04-26 10:15:32 +0200

    [#40129] Debug

--- src/os2datascanner/projects/report/asgi.py
+++ src/os2datascanner/projects/report/asgi.py
@@ -1,12 +1,14 @@
 import os
+import django
 
-from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
-from .reportapp import routing
+from os2datascanner.projects.report.reportapp import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'os2datascanner.projects.report.settings"')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'os2datascanner.projects.report.settings')
 django.setup()
+
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
   "http": AsgiHandler,
