3fc1fec9726b77af9e2dfe91c43246687461b6a0
Miklas Bøgvald <miklasbogvald@gmail.com>
2021-06-30 14:01:33 +0200

    Change HTTP server to ASGI (uvicorn) and add routing

---
+++ src/os2datascanner/projects/report/asgi.py
@@ -0,0 +1,20 @@
+import os
+import django
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from os2datascanner.projects.report.reportapp import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'os2datascanner.projects.report.settings')
+
+django.setup()
+from channels.auth import AuthMiddlewareStack
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+    URLRouter(
+      routing.websocket_urlpatterns
+    )
+  ),
+})
