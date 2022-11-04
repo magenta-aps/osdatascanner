283194ae8899ce2201e0453b3cd69517a99dabf5
Jesper <jdk@magenta-aps.dk>
2022-11-04 14:26:44 +0100

    [#52202] Styling and small changes.

--- src/os2datascanner/projects/report/reportapp/views/manual_views.py
+++ src/os2datascanner/projects/report/reportapp/views/manual_views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
 
+from ..models.roles.dpo import DataProtectionOfficer
+from ..utils import convert_context_to_email_body
+
 
 class ManualMainView(TemplateView):
     template_name = "manual.html"
