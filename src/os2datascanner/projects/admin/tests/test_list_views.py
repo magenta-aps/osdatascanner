c9c6ad11c6360533ce6a564ea6fcd7e427ad4279
Jesper <jdk@magenta-aps.dk>
2024-02-05 11:09:15 +0100

    [#59452] Test for searching for scannerjobs

--- src/os2datascanner/projects/admin/tests/test_list_views.py
+++ src/os2datascanner/projects/admin/tests/test_list_views.py
@@ -5,6 +5,7 @@ from parameterized import parameterized
 from django.contrib.auth.models import User, AnonymousUser
 from django.test import RequestFactory, TestCase
 from django.utils.text import slugify
+from django.urls import reverse_lazy
 
 from ..adminapp.views.webscanner_views import WebScannerList
 from ..adminapp.models.scannerjobs.webscanner import WebScanner
@@ -148,8 +149,23 @@ class ListViewsTest(TestCase):
             self.assertEqual(qs.first().organization.name, "IANA (example.com)")
         administrator.delete()
 
-    def listview_get_queryset(self, path, view):
-        request = self.factory.get(path)
+    def test_searching_for_scannerjobs(self):
+        # Arrange
+        self.user.is_superuser = True
+        self.user.save()
+
+        # Act
+        qs = self.listview_get_queryset(
+            reverse_lazy('webscanners'),
+            WebScannerList(),
+            request_kwargs={
+                'search_field': 'magenta'})
+
+        # Assert
+        self.assertEqual(qs.count(), 1)
+
+    def listview_get_queryset(self, path, view, **kwargs):
+        request = self.factory.get(path, data=kwargs.get('request_kwargs'))
         request.user = self.user
         view.setup(request)
         return view.get_queryset()
