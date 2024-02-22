06a5cd8e3da38d5e0cd16cbf150a53edc7c43a67
Jesper <jdk@magenta-aps.dk>
2024-02-22 14:57:09 +0100

    [#59847] Show imported accounts without positions in diagnostics

--- src/os2datascanner/projects/admin/adminapp/management/commands/diagnostics.py
+++ src/os2datascanner/projects/admin/adminapp/management/commands/diagnostics.py
@@ -46,12 +46,22 @@ class Command(BaseCommand):
         print("\n\n>> Running diagnostics on accounts ...")
         accounts = Account.objects.all()
         accounts_without_username = accounts.filter(username="").values("pk")
+        imported_accounts = accounts.filter(imported=True)
+        imported_accounts_no_positions = imported_accounts.filter(
+            positions__isnull=True).values("pk")
 
-        print(f"Found a total of {accounts.count()} accounts.")
+        print(
+            f"Found a total of {accounts.count()} accounts. "
+            f"{imported_accounts.count()} are imported.")
 
         if accounts_without_username:
             print(f"Found {len(accounts_without_username)} accounts without a username:", ", ".join(
-                [d["pk"] for d in accounts_without_username]))
+                [str(d["pk"]) for d in accounts_without_username]))
+
+        if imported_accounts_no_positions:
+            print(f"Found {len(imported_accounts_no_positions)} imported accounts without "
+                  "relation to an OrganizationalUnit:", ", ".join(
+                       [str(d["pk"]) for d in imported_accounts_no_positions]))
 
     def diagnose_aliases(self):
         print("\n\n>> Running diagnostics on aliases ...")
