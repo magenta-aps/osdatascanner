adaa12c0713b1a8cf5121b1236609f942fc14eb6
Alexander Faithfull <af@magenta.dk>
2024-02-23 09:01:13 +0100

    [#59850] Address the linter's latest complaint
    Guarding a single line with a single boolean member variable does not, in
    my assessment, make a function meaningfully more complex

--- src/os2datascanner/engine2/model/http.py
+++ src/os2datascanner/engine2/model/http.py
@@ -141,7 +141,7 @@ class WebSource(Source):
         # details from netloc
         return self
 
-    def handles(self, sm):
+    def handles(self, sm):  # noqa: CCR001
         session = sm.open(self)
         wc = crawler.WebCrawler(
                 self._url, session=session, timeout=TIMEOUT, ttl=TTL,
