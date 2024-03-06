ccb4a3906fed311bae8701c0d6d72ab8a1903e5a
Alexander Faithfull <af@magenta.dk>
2024-03-06 14:03:29 +0100

    [#60022] Add a cancel button to the new BackgroundJob widget

--- src/os2datascanner/projects/admin/core/urls.py
+++ src/os2datascanner/projects/admin/core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
             "background_job/<uuid:pk>/status",
             views.render_background_job_status_widget,
             name="background_job_status_widget"),
+    path(
+            "background_job/<uuid:pk>/cancel",
+            views.cancel_background_job,
+            name="background_job_cancel"),
 ]
--- src/os2datascanner/projects/admin/core/views.py
+++ src/os2datascanner/projects/admin/core/views.py
@@ -6,7 +6,7 @@ from django.template.response import SimpleTemplateResponse
 from .models.background_job import BackgroundJob
 
 
-def render_background_job_status_widget(pargh, pk: UUID):
+def render_background_job_status_widget(_, pk: UUID):
     try:
         context = {
             "job": BackgroundJob.objects.get(pk=pk)
@@ -14,5 +14,14 @@ def render_background_job_status_widget(pargh, pk: UUID):
         return SimpleTemplateResponse(
                 "components/background_job.html",
                 context=context)
+    except BackgroundJob.DoesNotExist:
+        raise Http404
+
+
+def cancel_background_job(_, pk: UUID):
+    try:
+        job = BackgroundJob.objects.get(pk=pk)
+        job.cancel()
+        return render_background_job_status_widget(_, pk)
     except BackgroundJob.DoesNotExist:
         raise Http404
