6b8d8bc82c2f0944328229850515f97a4d667461
Jonas Kofoed Hansen <jkh@magenta-aps.dk>
2024-03-26 16:59:30 +0100

    [#60264] Monkey found another

--- src/os2datascanner/engine2/tests/test_metadata.py
+++ src/os2datascanner/engine2/tests/test_metadata.py
@@ -1,5 +1,4 @@
 import os.path
-import unittest
 
 from os2datascanner.engine2.model.core import SourceManager
 from os2datascanner.engine2.model.file import (
@@ -14,56 +13,62 @@ from os2datascanner.engine2.model.derived.libreoffice import (
 test_data = FilesystemSource(os.path.join(os.path.dirname(__file__), "data"))
 
 
-class MetadataTest(unittest.TestCase):
+class TestMetadata:
     def test_odt_extraction(self):
+        # Arrange
         handle = LibreOfficeObjectHandle(
                 LibreOfficeSource(
                         FilesystemHandle(
                                 test_data, "libreoffice/embedded-cpr.odt")),
                 "embedded-cpr.html")
+        # Act
         with SourceManager() as sm:
             metadata = handle.follow(sm).get_metadata()
-        self.assertEqual(
-                metadata["od-creator"],
-                "Alexander John Faithfull",
-                "metadata extraction failed")
+
+        assert metadata["od-creator"] == "Alexander John Faithfull"
 
     def test_pdf_extraction(self):
+        # Arrange
         handle = PDFObjectHandle.make(
                 FilesystemHandle(test_data, "pdf/embedded-cpr.pdf"),
                 1, "page.txt")
+        # Act
         with SourceManager() as sm:
             metadata = handle.follow(sm).get_metadata()
-        self.assertEqual(
-                metadata["pdf-author"],
-                "Alexander John Faithfull",
-                "metadata extraction failed")
+
+        # Assert
+        assert metadata["pdf-author"] == "Alexander John Faithfull"
 
     def test_weird_pdf_metadata(self):
         """Null bytes in PDF metadata should be automatically removed."""
+        # Arrange
         handle = PDFPageHandle.make(
                 FilesystemHandle(test_data, "pdf/null-byte-in-author.pdf"), 1)
+        # Act
         with SourceManager() as sm:
             metadata = handle.follow(sm).get_metadata()
-        self.assertEqual(
-                metadata["pdf-author"],
-                "Alexander John Faithfull",
-                "metadata extraction failed")
+
+        # Assert
+        assert metadata["pdf-author"] == "Alexander John Faithfull"
 
     def test_no_author_pdf_metadata(self):
+        # Arrange
         handle = PDFPageHandle.make(
             FilesystemHandle(test_data, "pdf/null-byte-no-author.pdf"), 1)
 
+        # Act/Assert
         with SourceManager() as sm:
-            self.assertEqual(0, len([v for k, v in handle.follow(
-                sm)._generate_metadata()]))
+            assert len([v for k, v in handle.follow(sm)._generate_metadata()]) == 0
 
-    def test_web_domain_extraction(self):
+    def test_web_domain_extraction(self, monkeypatch):
+        # TODO: Should probably be generalized soon (also done in test_errors.py)
+        monkeypatch.setattr(
+            "os2datascanner.engine2.utilities.backoff.ExponentialBackoffRetrier._compute_delay", 0)
+
+        # Arrange/Act
         with SourceManager() as sm:
             metadata = WebHandle(
                     WebSource("https://www.example.invalid./"),
                     "/cgi-bin/test.pl").follow(sm).get_metadata()
-        self.assertEqual(
-                metadata.get("web-domain"),
-                "www.example.invalid.",
-                "web domain metadata missing")
+        # Assert
+        assert metadata.get("web-domain") == "www.example.invalid."
