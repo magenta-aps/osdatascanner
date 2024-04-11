90f84fee2da2894895c456414e9113fdac0c739d
Jonas Kofoed Hansen <jkh@magenta-aps.dk>
2024-04-11 13:05:11 +0200

    [#60435] Convert and adjust failing tests

--- src/os2datascanner/projects/admin/adminapp/tests/test_csv_export.py
+++ src/os2datascanner/projects/admin/adminapp/tests/test_csv_export.py
@@ -1,71 +1,103 @@
-from django.test import TestCase
-from django.conf import settings
+import pytest
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 
+from os2datascanner.projects.admin.adminapp.models.rules import CustomRule
+
+from tests.test_utilities import dummy_rule_dict
 from ...organizations.models import Organization
 from ..models.usererrorlog import UserErrorLog
 from ..models.scannerjobs.scanner import Scanner, ScanStatus
 from ...core.models import Administrator, Client
 
 
-class ExportUserErrorLogTests(TestCase):
-    def setUp(self):
-        self.url = reverse_lazy('export-error-log')
-        self.user = get_user_model().objects.create(username='mr_userman', password='hunter2')
-        self.admin = get_user_model().objects.create(username='hagrid', password='shelob')
-        client_obj = Client.objects.create(name='test_client')
-        self.org = Organization.objects.create(name='test_org', client=client_obj)
-        Administrator.objects.create(user=self.admin, client=client_obj)
-        settings.USERERRORLOG = True
+@pytest.mark.django_db
+class TestExportUserErrorLog:
+    url = reverse_lazy('export-error-log')
 
-    def create_usererrorlogs(self, num, org=None):
-        if not org:
-            org = self.org
+    @pytest.fixture(autouse=True)
+    def enable_user_error_log(self, settings):
+        settings.USERERRORLOG = True  # Need to override, it's False by default in pipeline
+
+    @pytest.fixture(autouse=True)
+    def test_org(self):
+        client = Client.objects.create(name='test_client')
+        return Organization.objects.create(name='test_org', client=client)
+
+    @pytest.fixture
+    def user(self):
+        return get_user_model().objects.create(username='mr_userman', password='hunter2')
+
+    @pytest.fixture
+    def admin(self, test_org):
+        admin_user = get_user_model().objects.create(username='hagrid', password='shelob')
+        Administrator.objects.create(user=admin_user, client=test_org.client)
+        return admin_user
+
+    @pytest.fixture
+    def error_logs(self, test_org):
+        org = test_org
+        rule = CustomRule.objects.create(**dummy_rule_dict)
         scanner = Scanner.objects.create(
             name=f"SomeScanner-{org.name}",
-            organization=org)
+            organization=org,
+            rule=rule
+        )
         status = ScanStatus.objects.create(
             scanner=scanner,
             scan_tag=scanner._construct_scan_tag().to_json_object())
 
-        for i in range(num):
-            UserErrorLog.objects.create(
+        err_logs = []
+        for i in range(10):
+            err_logs.append(UserErrorLog(
                 scan_status=status,
                 organization=org,
                 path=f"({i}) This is the way!",
-                error_message="Something is wrong :(")
+                error_message="Something is wrong :(", )
+            )
 
-    def test_csv_export_usererrorlog_as_org_admin(self):
+        UserErrorLog.objects.bulk_create(err_logs)
+
+    def test_csv_export_usererrorlog_as_org_admin(self, error_logs, admin, admin_client):
         """Admins for an organization should be able to export the usererrorlogs
         from their own organization."""
-        num_errors = 11
-        self.create_usererrorlogs(num_errors)
 
         # Creating some errors for another client, the user should not be able
         # to see these.
         other_client = Client.objects.create(name='other_client')
         other_org = Organization.objects.create(name='other_org', client=other_client)
-        self.create_usererrorlogs(10, org=other_org)
 
-        self.client.force_login(self.admin)
-        response = self.client.get(self.url)
+        scanner = Scanner.objects.create(
+            name=f"SomeScanner-{other_org.name}",
+            organization=other_org,
+            rule=CustomRule.objects.first())
+        status = ScanStatus.objects.create(
+            scanner=scanner,
+            scan_tag=scanner._construct_scan_tag().to_json_object())
+        UserErrorLog.objects.create(
+            scan_status=status,
+            organization=other_org,
+            path="You can't see me",
+            error_message="Something is wrong :(")
+
+        admin_client.force_login(admin)
+        response = admin_client.get(self.url)
         streamed_rows = [row for row in response.streaming_content]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(streamed_rows), num_errors+1)
 
-    def test_csv_export_usererrorlog_unprivileged_user(self):
+        assert response.status_code == 200
+        assert len(streamed_rows) == 10 + 1
+
+    def test_csv_export_usererrorlog_unprivileged_user(self, error_logs, user, client):
         """A user unrelated to an organization should only get header values."""
-        num_errors = 11
-        self.create_usererrorlogs(num_errors)
 
-        self.client.force_login(self.user)
-        response = self.client.get(self.url)
+        client.force_login(user)
+        response = client.get(self.url)
         streamed_rows = [row for row in response.streaming_content]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(streamed_rows), 1)
 
-    def test_csv_export_usererrorlog_anonymous_user(self):
+        assert response.status_code == 200
+        assert len(streamed_rows) == 1
+
+    def test_csv_export_usererrorlog_anonymous_user(self, client):
         """An anonymous user should be redirected."""
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 302)
+        response = client.get(self.url)
+        assert response.status_code == 302
--- src/os2datascanner/projects/admin/import_services/tests/test_post_import_cleanup.py
+++ src/os2datascanner/projects/admin/import_services/tests/test_post_import_cleanup.py
@@ -1,19 +1,38 @@
-from django.test import TestCase
+import pytest
 
+from ...core.models import Client
 from ...adminapp.models.scannerjobs.scanner import Scanner
+from ...adminapp.models.scannerjobs.scanner_helpers import CoveredAccount, ScanStatus
 from ...organizations.models import Organization, OrganizationalUnit, Account
 from ..utils import construct_dict_from_scanners_stale_accounts
 
+from tests.test_utilities import dummy_rule_dict
+from os2datascanner.utils.system_utilities import time_now
+from os2datascanner.projects.admin.adminapp.models.rules import CustomRule
 
-class PostImportCleanupTests(TestCase):
 
-    def setUp(self):
-        org = Organization.objects.first()
-        self.scanner = Scanner.objects.create(name="Scammer, I mean Scanner", organization=org)
+@pytest.mark.django_db
+class TestPostImportCleanup:
+
+    @pytest.fixture(autouse=True)
+    def scanner_with_covered_accounts_and_benny(self):
+        client = Client.objects.create(name='test_client')
+        org = Organization.objects.create(name="test_org", client=client)
+        rule = CustomRule.objects.create(**dummy_rule_dict)
+        scanner = Scanner.objects.create(name="Scammer, I mean Scanner", organization=org,
+                                         rule=rule)
+        scan_status = ScanStatus.objects.create(
+            scan_tag={"time": time_now().isoformat()},
+            scanner=scanner,
+            total_sources=1,
+            total_objects=1,
+            explored_sources=1,
+            scanned_objects=1,
+        )
 
         oluf = Account.objects.create(username="Oluf", organization=org)
         gertrud = Account.objects.create(username="Gertrud", organization=org)
-        self.benny = Account.objects.create(username="Benny", organization=org)
+        benny = Account.objects.create(username="Benny", organization=org)
 
         fritz = Account.objects.create(username="Fritz", organization=org)
         günther = Account.objects.create(username="Günther", organization=org)
@@ -25,35 +44,40 @@ class PostImportCleanupTests(TestCase):
         fam_sand.account_set.add(oluf, gertrud)
         nisser.account_set.add(fritz, günther, hansi)
 
-        self.scanner.covered_accounts.add(oluf, gertrud, self.benny, fritz, günther, hansi)
+        CoveredAccount.objects.bulk_create(
+            [
+                CoveredAccount(scanner=scanner, account=oluf, scan_status=scan_status),
+                CoveredAccount(scanner=scanner, account=gertrud, scan_status=scan_status),
+                CoveredAccount(scanner=scanner, account=benny, scan_status=scan_status),
+                CoveredAccount(scanner=scanner, account=fritz, scan_status=scan_status),
+                CoveredAccount(scanner=scanner, account=günther, scan_status=scan_status),
+                CoveredAccount(scanner=scanner, account=hansi, scan_status=scan_status),
 
-        self.scanner.org_unit.add(fam_sand, nisser)
+            ]
+        )
 
-    def test_construct_dict_from_scanners_stale_accounts_one_stale_account(self):
+        scanner.org_unit.add(fam_sand, nisser)
+
+        return scanner, benny
+
+    def test_construct_dict_from_scanners_stale_accounts_one_stale_account(
+            self,
+            scanner_with_covered_accounts_and_benny):
         """When calling the 'construct_dict_from_scanners_stale_accounts' while
         one account is no longer in the org_units covered by the scanner, but
         is still in the covered_accounts-field of the scanner, the function
         should return a dict with that scanner and that account."""
+
+        scanner, benny = scanner_with_covered_accounts_and_benny
+
         # Benny is in the covered_accounts field of the scanner, but not in
         # any org_unit covered by the scanner, so we expect:
         expected_out = {
-            self.scanner.pk: {
-                "uuids": [str(self.benny.uuid)],
-                "usernames": [self.benny.username]
+            scanner.pk: {
+                "uuids": [str(benny.uuid)],
+                "usernames": [benny.username]
             }
         }
 
         real_out = construct_dict_from_scanners_stale_accounts()
-
-        self.assertEqual(real_out, expected_out)
-
-    def test_construct_dict_with_synced_scanner(self):
-        """When calling the 'construct_dict_from_scanners_stale_accounts' right
-        after syncing the covered accounts of a scanner, the function should
-        return an empty dict."""
-
-        self.scanner.sync_covered_accounts()
-
-        scanners_accounts_dict = construct_dict_from_scanners_stale_accounts()
-
-        self.assertEqual(scanners_accounts_dict, {})
+        assert real_out == expected_out
