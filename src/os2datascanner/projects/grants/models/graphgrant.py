03c3605c970a65f78dfdfef3d79a446f62199fea
Alexander Faithfull <af@magenta.dk>
2024-05-15 18:04:57 +0200

    [#60860] Move app ID and client secret tracking to GraphGrant
    At the moment, this is done largely to pave the way for the futuristic
    world of synchronised Grants; I still have no idea what the UI of this
    will eventually look like

--- src/os2datascanner/projects/grants/models/graphgrant.py
+++ src/os2datascanner/projects/grants/models/graphgrant.py
@@ -2,8 +2,10 @@ from uuid import uuid4
 from django.db import models
 
 from os2datascanner.engine2.model.msgraph.utilities import make_token
-from os2datascanner.projects.admin import settings
+from os2datascanner.projects.utils import aes
 from .grant import Grant
+
+from django.conf import settings
 
 
 class GraphGrant(Grant):
@@ -13,14 +15,33 @@ class GraphGrant(Grant):
     (Note that the specific permissions associated with this entitlement are
     not specified here, but in the OS2datascanner application registration in
     Microsoft's portal.)"""
+    app_id = models.UUIDField(
+            default=uuid4, editable=False, verbose_name="app ID")
+
     tenant_id = models.UUIDField(
             default=uuid4, editable=False, verbose_name="tenant ID")
 
+    _client_secret = models.JSONField(verbose_name="client secret")
+
+    @classmethod
+    def encrypt_secret(cls, secret: str) -> (str, str):
+        return tuple(
+                c.hex() for c in aes.encrypt(secret, settings.DECRYPTION_HEX))
+
+    @property
+    def client_secret(self) -> str:
+        iv, ciphertext = [bytes.fromhex(c) for c in self._client_secret]
+        return aes.decrypt(iv, ciphertext, settings.DECRYPTION_HEX)
+
+    @client_secret.setter
+    def client_secret(self, secret: str):
+        self._client_secret = self.encrypt_secret(secret)
+
     def make_token(self):
         return make_token(
-                settings.MSGRAPH_APP_ID,
+                self.app_id,
                 str(self.tenant_id),
-                settings.MSGRAPH_CLIENT_SECRET)
+                self.client_secret)
 
     def validate(self):
         return self.make_token() is not None
