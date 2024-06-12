ec65b32dc0127e5c5d37c0782756cd599a6d57e8
Alexander Faithfull <af@magenta.dk>
2024-06-12 16:42:57 +0200

    [#61139] Add the new Grant subclasses to the grants app's admin pages
    This commit also slightly adjusts the treatment of encrypted fields:
    instead of dumping the initialisation vector and ciphertext, we now print
    a simple summary of whether or not the field has a defined value. (I'd
    like to do more here, but I still don't understand Django Forms well
    enough...)

--- src/os2datascanner/projects/grants/admin.py
+++ src/os2datascanner/projects/grants/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
-from .models import GraphGrant
+from .models import EWSGrant, SMBGrant, GraphGrant
 
 
-admin.site.register(GraphGrant)
+@admin.register(EWSGrant)
+class EWSGrantAdmin(admin.ModelAdmin):
+    fields = ["organization", "username", "password"]
+    readonly_fields = ["password"]
+
+    @admin.display(description=_("password"))
+    def password(self, obj):
+        return format_html(
+                "<em>{}</em>",
+                _("defined") if obj._password else _("not defined"))
+
+
+@admin.register(SMBGrant)
+class SMBGrantAdmin(admin.ModelAdmin):
+    fields = ["organization", "domain", "username", "password"]
+    readonly_fields = ["password"]
+
+    @admin.display(description=_("password"))
+    def password(self, obj):
+        return format_html(
+                "<em>{}</em>",
+                _("defined") if obj._password else _("not defined"))
+
+
+@admin.register(GraphGrant)
+class GraphGrantAdmin(admin.ModelAdmin):
+    fields = ["organization", "app_id", "tenant_id", "client_secret"]
+    readonly_fields = ["client_secret"]
+
+    @admin.display(description=_("client secret"))
+    def client_secret(self, obj):
+        return format_html(
+                "<em>{}</em>",
+                _("defined") if obj._client_secret else _("not defined"))
