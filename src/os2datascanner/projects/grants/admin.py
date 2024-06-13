cb3115b963a7369520d282b955213b8ab16c4e18
Alexander Faithfull <af@magenta.dk>
2024-06-13 13:25:13 +0200

    [#61139] Implement an auto encryption widget for the grants admin pages
    I wouldn't have even tried this if not for the MR process giving me a
    minute to think, so hooray, code review saves the day

--- src/os2datascanner/projects/grants/admin.py
+++ src/os2datascanner/projects/grants/admin.py
@@ -1,41 +1,94 @@
+from django import forms
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.contrib import admin
-from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
+from os2datascanner.projects.utils import aes
 from .models import EWSGrant, SMBGrant, GraphGrant
+
+
+class AutoEncryptedField(forms.CharField):
+    widget = forms.widgets.PasswordInput
+
+    def prepare_value(self, value):
+        if value:
+            self.widget.attrs["placeholder"] = _("(unchanged)")
+        else:
+            pass
+
+    def to_python(self, value):
+        if value:
+            return [c.hex()
+                    for c in aes.encrypt(value, settings.DECRYPTION_HEX)]
+
+
+def choose_field_value(new, old):
+    match new, old:
+        case new, _ if new:
+            return new
+        case None, old if old:
+            return old
+        case None, None:
+            raise ValidationError(_("This field is required."))
+
+
+class EWSGrantForm(forms.ModelForm):
+    model = EWSGrant
+
+    username = forms.CharField()  # Better than the default AdminTextField
+    _password = AutoEncryptedField(required=False)
+
+    def clean__password(self):
+        return choose_field_value(
+              self.cleaned_data["_password"],
+              self.instance._password)
 
 
 @admin.register(EWSGrant)
 class EWSGrantAdmin(admin.ModelAdmin):
-    fields = ["organization", "username", "password"]
-    readonly_fields = ["password"]
+    fields = ["organization", "username", "_password"]
+    form = EWSGrantForm
 
-    @admin.display(description=_("password"))
-    def password(self, obj):
-        return format_html(
-                "<em>{}</em>",
-                _("defined") if obj._password else _("not defined"))
+
+class SMBGrantForm(forms.ModelForm):
+    model = SMBGrant
+
+    username = forms.CharField()
+    domain = forms.CharField(required=False)
+    _password = AutoEncryptedField(required=False)
+
+    def clean__password(self):
+        return choose_field_value(
+              self.cleaned_data["_password"],
+              self.instance._password)
 
 
 @admin.register(SMBGrant)
 class SMBGrantAdmin(admin.ModelAdmin):
-    fields = ["organization", "domain", "username", "password"]
-    readonly_fields = ["password"]
+    fields = ["organization", "domain", "username", "_password"]
+    form = SMBGrantForm
 
-    @admin.display(description=_("password"))
-    def password(self, obj):
-        return format_html(
-                "<em>{}</em>",
-                _("defined") if obj._password else _("not defined"))
+
+class GraphGrantForm(forms.ModelForm):
+    model = GraphGrant
+
+    _client_secret = AutoEncryptedField(required=False)
+
+    def clean__client_secret(self):
+        return choose_field_value(
+              self.cleaned_data["_client_secret"],
+              self.instance._client_secret)
 
 
 @admin.register(GraphGrant)
 class GraphGrantAdmin(admin.ModelAdmin):
-    fields = ["organization", "app_id", "tenant_id", "client_secret"]
-    readonly_fields = ["client_secret"]
+    fields = ["organization", "app_id", "tenant_id", "_client_secret"]
+    form = GraphGrantForm
 
-    @admin.display(description=_("client secret"))
-    def client_secret(self, obj):
-        return format_html(
-                "<em>{}</em>",
-                _("defined") if obj._client_secret else _("not defined"))
+    def get_changeform_initial_data(self, request):
+        # Suppress the random UUID values GraphGrants have by default
+        return {
+            "app_id": "",
+            "tenant_id": ""
+        }
