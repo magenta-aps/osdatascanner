06fea22149b75530bb62ca30344ca0fa54c5c106
Andreas Kring <andreas@magenta.dk>
2024-06-14 10:54:32 +0200

    [#55441] Add unit test for _retry_post_query

--- src/os2datascanner/projects/admin/import_services/tests/os2mo_import/test_os2mo_import_job.py
+++ src/os2datascanner/projects/admin/import_services/tests/os2mo_import/test_os2mo_import_job.py
@@ -1,3 +1,8 @@
+from unittest.mock import MagicMock
+
+from django.conf import settings
+from requests import HTTPError
+
 from os2datascanner.projects.admin.import_services.models.os2mo_import_job import OS2moImportJob
 
 GQL_RESPONSE = {
@@ -33,3 +38,39 @@ def test__get_org_unit_data():
             "some": "mo unit 2",
         }
     ]
+
+
+def test__retry_post_query():
+    # Arrange
+    mo_response = MagicMock()
+    mo_response.json.return_value = {"foo": "bar"}
+
+    mock_session = MagicMock()
+    mock_session.post.side_effect = [HTTPError(), mo_response]
+
+    # Act
+    r = OS2moImportJob._retry_post_query(
+        mock_session,
+        "fake_token",
+        "https://os2mo.magenta.dk/graphql",
+        next_cursor="some_cursor",
+    )
+
+    # Assert
+    assert mock_session.post.call_count == 2
+    mock_session.post.assert_called_with(
+        "https://os2mo.magenta.dk/graphql",
+        json={
+            "query": OS2moImportJob.QueryOrgUnitsManagersEmployees,
+            "variables": {
+                "cursor": "some_cursor",
+                "limit": settings.OS2MO_PAGE_SIZE,
+                "email_type": settings.OS2MO_EMAIL_ADDRESS_TYPE,
+            }
+        },
+        headers={
+            "content-type": "application/json; charset=UTF-8",
+            "authorization": "Bearer fake_token"
+        }
+    )
+    assert r == {"foo": "bar"}
