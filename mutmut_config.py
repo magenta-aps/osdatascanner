a3089dc0e99b8e4293a02ea1a54509cc3186e11d
Jesper <jdk@magenta-aps.dk>
2024-06-28 15:01:57 +0200

    [#56492] Skip logging statements during mutation

--- mutmut_config.py
+++ mutmut_config.py
@@ -3,3 +3,8 @@ def pre_mutation(context):
     # testing on. Remember to change the [mutmut]-settings in setup.cfg as well.
     path = "organizations/tests/test_account_outlook_setting.py"
     context.config.test_command = "docker compose exec report pytest " + path
+
+    # We don't test for log statements, so skip those lines during mutation.
+    line = context.current_source_line.strip()
+    if line.startswith('log.'):
+        context.skip = True
