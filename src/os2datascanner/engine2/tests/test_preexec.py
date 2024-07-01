59ec55796c9992225c5341a58b40565c6248ac52
Alexander Faithfull <af@magenta.dk>
2024-07-01 11:32:38 +0200

    [#41168] Test that the pipeline's explorer stage can pre-execute Rules

--- src/os2datascanner/engine2/tests/test_preexec.py
+++ src/os2datascanner/engine2/tests/test_preexec.py
@@ -1,10 +1,28 @@
+from os.path import join as joinpath
+from time import sleep
 import pytest
+from tempfile import TemporaryDirectory
 
+from os2datascanner.utils.system_utilities import time_now
+from os2datascanner.engine2.model import file
+from os2datascanner.engine2.model.core import SourceManager
 from os2datascanner.engine2.rules import logical
+from os2datascanner.engine2.rules.last_modified import LastModifiedRule
 from os2datascanner.engine2.rules.utilities.analysis import compute_mss
+from os2datascanner.engine2.pipeline import explorer, messages
 
 
 class TestPreExec:
+    @staticmethod
+    def make_message_base(td):
+        return messages.ScanSpecMessage(
+                scan_tag=messages.ScanTagFragment.make_dummy(),
+                source=file.FilesystemSource(td),
+                rule=None,
+                configuration={},
+                progress=None,
+                filter_rule=None)
+
     @pytest.mark.parametrize(
             "rule,mss",
             [
@@ -38,3 +56,37 @@ class TestPreExec:
     def test_mss_computation(self, rule, mss):
         """The minimal set of SimpleRules is calculated correctly."""
         assert compute_mss(rule) == mss
+
+    def test_explorer_rule(self):
+        """The pipeline's explorer stage correctly propagates rules to Sources
+        for pre-execution."""
+        with SourceManager() as sm, TemporaryDirectory() as td:
+            # Arrange
+            with open(joinpath(td, "test_one.txt"), "wt") as fp:
+                fp.write("This is the first test file...")
+            with open(joinpath(td, "test_two.txt"), "wt") as fp:
+                fp.write("... this is the second test file...")
+
+            # Sleep for a second to make sure that the first two files and
+            # the last one have differentiable timestamps
+            sleep(1)
+
+            after_first_two = time_now()
+
+            with open(joinpath(td, "test_three.txt"), "wt") as fp:
+                fp.write("... and this is the FINAL TEST FILE.")
+
+            message = self.make_message_base(td)._replace(
+                    rule=LastModifiedRule(after=after_first_two))
+
+            # Act
+            message_objects = [
+                    messages.ConversionMessage.from_json_object(j)
+                    for channel, j in explorer.message_received_raw(
+                            message.to_json_object(),
+                            "os2ds_scan_specs", sm)
+                    if channel == "os2ds_conversions"]
+            handle_names = set(msg.handle.name for msg in message_objects)
+
+            # Assert
+            assert handle_names == {"test_three.txt"}
