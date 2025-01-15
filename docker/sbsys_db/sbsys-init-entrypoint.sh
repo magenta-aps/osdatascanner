db9e62cc5f71d78ea335e735481a0568a838033a
Alexander Faithfull <af@magenta.dk>
2025-01-15 14:39:35 +0100

    [#63442] Make it easier to add new .sql files to the SBSYS DB init process
    The entrypoint now sources all the .sql files in the docker/sbsys_db/
    folder with a shell glob

--- docker/sbsys_db/sbsys-init-entrypoint.sh
+++ docker/sbsys_db/sbsys-init-entrypoint.sh
@@ -2,7 +2,12 @@
 
 # Initialize the DB structure (the password used is not sensitive as it
 # is only use in the development environment and in the GitLab CI pipeline)
-/opt/mssql-tools18/bin/sqlcmd -C -S SBSYS_DB -U sa -P ub3rStr0nGpassword -d master -i /db_init.sql;
+for i in /code/*.sql; do
+    /opt/mssql-tools18/bin/sqlcmd \
+        -C -S SBSYS_DB \
+        -U sa -P ub3rStr0nGpassword \
+        -d master -i "$i"
+done
 
 # Populate the SBSYS DB (for development and testing)
 python -m os2datascanner.integrations.sbsys.dev_env.db_init
