0c9a75292901dbdf5cfc22dbf017f211a6d6c4b3
jdk <jdk@magenta-aps.dk>
2025-01-24 12:39:18 +0100

    [#63626] Use pytest in test_accounts.py

--- src/os2datascanner/projects/admin/organizations/tests/test_accounts.py
+++ src/os2datascanner/projects/admin/organizations/tests/test_accounts.py
@@ -1,21 +1,13 @@
 import pytest
 
-from django.test import TestCase
 from django.db.utils import IntegrityError
 
-from parameterized import parameterized
-
-from ...core.models.client import Client
-from ...adminapp.models.scannerjobs.scanner_helpers import ScanStatus
-from ..models import OrganizationalUnit, Organization, Account
-from ...adminapp.models.scannerjobs.scanner import Scanner
-from ...adminapp.models.rules import CustomRule
-from ...tests.test_utilities import dummy_rule_dict
-from os2datascanner.utils.system_utilities import time_now
+from ..models import Account
 
 
 @pytest.mark.django_db
 class TestAccounts:
+
     def test_account_username_org_constraint(self, test_org, oluf):
         with pytest.raises(IntegrityError):
             Account.objects.create(
@@ -24,78 +16,18 @@ class TestAccounts:
             )
 
 
-class AccountMethodTests(TestCase):
+class TestAccountMethods:
 
-    def setUp(self):
-        # Create new org for the sake of these tests.
-        self.client = Client.objects.create(
-            name="OS2datascanner Test",
-            contact_email="info@magenta-aps.dk",
-            contact_phone="+45 3336 9696")
-
-        self.org = Organization.objects.create(
-            name="OS2datascanner Test",
-            contact_email="info@magenta-aps.dk",
-            contact_phone="+45 3336 9696",
-            client_id=self.client.uuid,
-            slug="os2datascanner-test")
-
-        # Create dummy rule for the sake of the tests
-        self.rule = CustomRule.objects.create(**dummy_rule_dict)
-
-        # Create objects
-        self.unit1 = OrganizationalUnit.objects.create(
-            name="Unit1", organization=self.org)
-        self.unit2 = OrganizationalUnit.objects.create(
-            name="Unit2", organization=self.org)
-        self.scanner1 = Scanner.objects.create(
-            name="Hansi & Günther", organization=self.org, rule=self.rule)
-        self.scanner2 = Scanner.objects.create(
-            name="Hansi", organization=self.org, rule=self.rule)
-        self.hansi = Account.objects.create(username="Hansi", organization=self.org)
-        self.günther = Account.objects.create(username="Günther", organization=self.org)
-        self.fritz = Account.objects.create(username="Fritz", organization=self.org)
-        # Avoid having an identical timestamps
-        self.status1 = ScanStatus.objects.create(
-            scan_tag={"time": time_now().replace(hour=11).isoformat()},
-            scanner=self.scanner1
-        )
-        self.status2 = ScanStatus.objects.create(
-            scan_tag={"time": time_now().replace(hour=12).isoformat()},
-            scanner=self.scanner2
-        )
-
-        # Assign accounts to a unit
-        self.unit1.account_set.add(self.hansi, self.günther)
-        self.unit2.account_set.add(self.hansi)
-
-    def tearDown(self):
-        # Clean up this mess...
-        self.fritz.delete()
-        self.günther.delete()
-        self.hansi.delete()
-
-        self.scanner2.delete()
-        self.scanner1.delete()
-
-        self.unit2.delete()
-        self.unit1.delete()
-
-        self.org.delete()
-        self.client.delete()
-
-        self.rule.delete()
-
-    @parameterized.expand([
+    @pytest.mark.parametrize("account,initials", [
         (Account(username="superman", first_name="Clark", last_name="Kent"), "CK"),
         (Account(username="robin", first_name="Robin"), "R"),
         (Account(username="batman", last_name="Wayne"), "W"),
         (Account(username="wonder_woman"), None)
     ])
-    def test_initials(self, account, expected_initials):
+    def test_initials(self, account, initials):
         """The 'initials'-method should return the first letter from the first
         and the last name. If the account only has a first name, only one
         letter should be returned. If the account has no names, None should be
         returned."""
 
-        self.assertEqual(account.initials, expected_initials)
+        assert account.initials == initials
