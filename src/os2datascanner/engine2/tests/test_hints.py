667245d5c5e6b0f90b277e0c30a2935e933d8fa4
Asbjørn Lind <asl@magenta-aps.dk>
2025-02-13 14:33:40 +0100

    [#63873] Convert test_hints to pytest

--- src/os2datascanner/engine2/tests/test_hints.py
+++ src/os2datascanner/engine2/tests/test_hints.py
@@ -1,44 +1,43 @@
-import unittest
+import pytest
 
 from os2datascanner.engine2.model.smbc import SMBCSource, SMBCHandle
 
 
-class HintTests(unittest.TestCase):
-    def setUp(self):
-        self._hint_dict = {
-            "biscuits": "tasty",
-            "pet_count": {
-                "dog": 4,
-                "cat": 3.14159,  # one very round cat(?)
-                "gerbil": 3,
-            }
+@pytest.fixture
+def hint_dict():
+    return {
+        "biscuits": "tasty",
+        "pet_count": {
+            "dog": 4,
+            "cat": 3.14159,  # one very round cat(?)
+            "gerbil": 3,
         }
+    }
 
-        self._source = SMBCSource(
-                "//SERVER/Resource", "username", "topsecret", "WORKGROUP8")
-        self._handle = SMBCHandle(
-                self._source, "~ocument.docx", hints=self._hint_dict)
 
-    def test_hint_serialisation(self):
+@pytest.fixture
+def source():
+    return SMBCSource("//SERVER/Resource", "username", "topsecret", "WORKGROUP8")
+
+
+@pytest.fixture
+def handle(source, hint_dict):
+    return SMBCHandle(source, "~ocument.docx", hints=hint_dict)
+
+
+class TestHint:
+    def test_hint_serialisation(self, handle, hint_dict):
         """Hints survive serialisation and deserialisation."""
-        rtd_handle = SMBCHandle.from_json_object(
-                self._handle.to_json_object())
+        rtd_handle = SMBCHandle.from_json_object(handle.to_json_object())
 
-        for k in self._hint_dict.keys():
-            hint = self._handle.hint(k)
-            self.assertIsNotNone(
-                    hint,
-                    "hint incorrectly stored in original object")
-            self.assertEqual(
-                    hint,
-                    rtd_handle.hint(k),
-                    "hint did not survive JSON round trip")
+        for k in hint_dict.keys():
+            hint = handle.hint(k)
+            assert hint is not None
+            assert hint == rtd_handle.hint(k)
 
-    def test_hint_deletion(self):
+    def test_hint_deletion(self, handle, hint_dict):
         """Hints associated with an object can be deleted."""
-        self._handle.clear_hints()
+        handle.clear_hints()
 
-        for k in self._hint_dict.keys():
-            self.assertIsNone(
-                    self._handle.hint(k),
-                    "hint survived deletion")
+        for k in hint_dict.keys():
+            assert handle.hint(k) is None
