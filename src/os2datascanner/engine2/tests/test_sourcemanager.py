36c69ed551f09917976f87259a4e1417b67a0112
Asbjørn Lind <asl@magenta-aps.dk>
2025-02-13 13:34:55 +0100

    [#63873] Convert test_sourcemanager to pytest

--- src/os2datascanner/engine2/tests/test_sourcemanager.py
+++ src/os2datascanner/engine2/tests/test_sourcemanager.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from os2datascanner.engine2.model.core import SourceManager
 
@@ -36,20 +36,14 @@ class BrokenSource:
         yield from []
 
 
-class Engine2SourceManagerTest(unittest.TestCase):
+class TestEngine2SourceManager:
     def test_basic(self):
         tracker = Tracker()
         with SourceManager() as sm:
             sm.open(tracker)
             sm.open(tracker)
-            self.assertEqual(
-                    tracker.count,
-                    1,
-                    "SourceManager opened the same object twice")
-        self.assertEqual(
-                tracker.count,
-                0,
-                "SourceManager didn't close the object")
+            assert tracker.count == 1, "SourceManager opened the same object twice"
+        assert tracker.count == 0, "SourceManager didn't close the object"
 
     def test_dependencies(self):
         tracker1 = Tracker()
@@ -60,29 +54,15 @@ class Engine2SourceManagerTest(unittest.TestCase):
             sm.open(tracker2)
             sm.open(dependent)
 
-            self.assertEqual(
-                    dependent.count,
-                    1,
-                    "SourceManager didn't open the dependent")
+            assert dependent.count == 1, "SourceManager didn't open the dependent"
 
             sm.close(tracker1)
 
-            self.assertEqual(
-                    dependent.count,
-                    0,
-                    "SourceManager didn't close the dependent")
-            self.assertEqual(
-                    tracker1.count,
-                    0,
-                    "SourceManager didn't close the parent object")
-            self.assertEqual(
-                    tracker2.count,
-                    1,
-                    "SourceManager closed an unrelated object")
-        self.assertEqual(
-                tracker2.count,
-                0,
-                "SourceManager didn't eventually close the unrelated object")
+            assert dependent.count == 0, "SourceManager didn't close the dependent"
+            assert tracker1.count == 0, "SourceManager didn't close the parent object"
+            assert tracker2.count == 1, "SourceManager closed an unrelated object"
+
+        assert tracker2.count == 0, "SourceManager didn't eventually close the unrelated object"
 
     def test_width(self):
         tracker1 = Tracker()
@@ -92,21 +72,15 @@ class Engine2SourceManagerTest(unittest.TestCase):
             sm.open(tracker1)
             sm.open(tracker2)
 
-            self.assertEqual(
-                    tracker1.count,
-                    1)
-            self.assertEqual(
-                    tracker2.count,
-                    1)
+            assert tracker1.count == 1
+            assert tracker2.count == 1
+            assert tracker3.count == 0
 
             sm.open(tracker3)
 
-            self.assertEqual(
-                    tracker1.count,
-                    0)
-            self.assertEqual(
-                    tracker3.count,
-                    1)
+            assert tracker1.count == 0
+            assert tracker2.count == 1
+            assert tracker3.count == 1
 
     def test_nested_lru(self):
         tracker1 = Tracker()
@@ -125,9 +99,7 @@ class Engine2SourceManagerTest(unittest.TestCase):
             sm.open(tracker4)
             sm.open(tracker3)
 
-            self.assertEqual(
-                    tracker2.count,
-                    0)
+            assert tracker2.count == 0
 
     def test_dependent_clearing(self, width=None):
         tracker1 = Tracker()
@@ -143,34 +115,25 @@ class Engine2SourceManagerTest(unittest.TestCase):
 
             sm.clear_dependents()
             for indie in (tracker1, tracker2, tracker3,):
-                self.assertEqual(
-                        indie.count,
-                        1,
-                        f"independent Source {indie} was closed")
+                assert indie.count == 1, f"independent Source {indie} was closed"
             for dependent in (tracker1a, tracker3a, tracker3aa,):
-                self.assertEqual(
-                        dependent.count,
-                        0,
-                        f"dependent Source {dependent} was not closed")
+                assert dependent.count == 0, f"dependent Source {dependent} was not closed"
 
-    # XXX: revisit this!
-    @unittest.skip("SourceManager.width bug")
+    @pytest.mark.skip(reason="SourceManager.width bug")
     def test_width_with_depth(self):
         self.test_dependent_clearing(width=3)
 
     def test_generator_exception(self):
         source = BrokenSource()
         with SourceManager() as sm:
-            with self.assertRaises(ValueError):
+            with pytest.raises(ValueError):
                 sm.open(source)
-            self.assertFalse(
-                    source in sm,
-                    "_generate_state failed, but Source still open")
+            assert source not in sm, "_generate_state failed, but Source still open"
 
     def test_generator_exception2(self):
         source = BrokenSource()
         with SourceManager() as sm:
-            with self.assertRaises(ValueError):
+            with pytest.raises(ValueError):
                 sm.open(source)
-            with self.assertRaises(ValueError):
+            with pytest.raises(ValueError):
                 sm.open(source)
