5a1b721f43a6bd31742d29b7440344cc3d160def
Asbjørn Lind <asl@magenta-aps.dk>
2025-02-13 13:26:25 +0100

    [#63873] Conver test_tempfiles to pytest

--- src/os2datascanner/engine2/tests/test_tempfiles.py
+++ src/os2datascanner/engine2/tests/test_tempfiles.py
@@ -1,34 +1,20 @@
-import unittest
 import os.path
 
 from os2datascanner.engine2.model.utilities import NamedTemporaryResource
 
 
-test_string = "T e s t\ns t r i n g."
-
-
-class Engine2TempfilesTest(unittest.TestCase):
+class TestEngine2TempfilesTest:
     def test(self):
+        test_string = "T e s t\ns t r i n g."
         with NamedTemporaryResource("test.txt") as ntr:
             path = ntr.get_path()
             directory = os.path.basename(path)
-            self.assertFalse(
-                    os.path.exists(path),
-                    "temp file {0} shouldn't exist yet".format(path))
+            assert not os.path.exists(path), f"temp file {path} shouldn't exist yet"
             with ntr.open("wt") as fp:
                 fp.write(test_string)
-            self.assertTrue(
-                    os.path.exists(path),
-                    "temp file {0} was not created".format(path))
+            assert os.path.exists(path), f"temp file {path} was not created"
             with open(path, "rt") as fp:
                 content = fp.read()
-                self.assertEqual(
-                        content,
-                        test_string,
-                        "temp file content '{0}' is incorrect".format(content))
-        self.assertFalse(
-                os.path.exists(path),
-                "temp file {0} wasn't cleaned up".format(path))
-        self.assertFalse(
-                os.path.exists(directory),
-                "temp directory {0} wasn't cleaned up".format(directory))
+                assert content == test_string, f"temp file content '{content}' is incorrect"
+        assert not os.path.exists(path), f"temp file {path} wasn't cleaned up"
+        assert not os.path.exists(directory), f"temp directory {0} wasn't cleaned up"
