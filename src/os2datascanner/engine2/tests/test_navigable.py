3e8ee44916f60f70e1d77b8710db65c2552e0e86
Asbjørn Lind <asl@magenta-aps.dk>
2025-02-13 15:22:23 +0100

    [#63873] Convert test_navigable to pytest

--- src/os2datascanner/engine2/tests/test_navigable.py
+++ src/os2datascanner/engine2/tests/test_navigable.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from os2datascanner.engine2.conversions.utilities import navigable
 
@@ -24,61 +24,41 @@ values = (
         (1, 5, 7, 10), {1, 5, 8, 7})
 
 
-class NavigableTest(unittest.TestCase):
-    def test_promotion(self):
+class TestNavigable:
+    @pytest.mark.parametrize("value", values)
+    def test_promotion(self, value):
         """Basic Python values can be promoted to navigable ones while
         preserving their original type."""
-        for k in values:
-            navi = navigable.make_navigable(k)
-            self.assertEqual(
-                    navi,
-                    k,
-                    "value did not survive promotion")
-            self.assertIsInstance(
-                    navi,
-                    type(k),
-                    "type has changed too much")
-            self.assertTrue(
-                    hasattr(navi, "parent"),
-                    "parent attribute is missing")
-            self.assertIsInstance(
-                    navi,
-                    navigable._NavigableBase,
-                    "type is not navigable")
+        navi = navigable.make_navigable(value)
+        assert navi == value
+        assert isinstance(navi, type(value))
+        assert hasattr(navi, "parent")
+        assert isinstance(navi, navigable._NavigableBase)
 
     def test_linked_promotion(self):
         """The parent of a navigable value is tracked."""
         parent = {}
         parent["k"] = navigable.make_navigable(1, parent=parent)
-        self.assertIs(
-                parent,
-                parent["k"].parent,
-                "navigable has the wrong parent")
+        assert parent is parent["k"].parent
 
     def test_bulk_promotion(self):
         """The values of a dict can be converted to navigable ones."""
         mapped_values = {type(value).__name__: value for value in values}
         navi = navigable.make_values_navigable(mapped_values)
         for v in navi.values():
-            self.assertIs(
-                    navi,
-                    v.parent,
-                    "navigable has the wrong parent")
+            assert navi is v.parent
 
     def test_failed_promotion(self):
         """A value with no corresponding navigable type cannot be made
         navigable."""
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             navigable.make_navigable(Dummy())
 
     def test_promotion_registration(self):
         """New types can be made navigable."""
         navigable.make_navigable_type(Dummy2)
         dummy = navigable.make_navigable(Dummy2())
-        self.assertIsInstance(
-                dummy,
-                navigable._NavigableBase,
-                "type is not navigable")
+        assert isinstance(dummy, navigable._NavigableBase)
 
     def test_adapter_registration(self):
         """Types without a one-argument copy constructor can also be made
@@ -87,7 +67,10 @@ class NavigableTest(unittest.TestCase):
                 Dummy3, adaptor=lambda dummy: (dummy.x, dummy.y, dummy.z))
         dummy = Dummy3(5, 10, 20)
         navigable_dummy = navigable.make_navigable(dummy)
-        self.assertEqual(
-                (dummy.x, dummy.y, dummy.z),
-                (navigable_dummy.x, navigable_dummy.y, navigable_dummy.z),
-                "adaptor failed")
+        assert (
+            dummy.x,
+            dummy.y,
+            dummy.z) == (
+            navigable_dummy.x,
+            navigable_dummy.y,
+            navigable_dummy.z)
