d74469c0a6ffe8c071ed8d6cc4f2931e65324c80
Asbjørn Lind <asl@magenta-aps.dk>
2025-02-13 15:05:23 +0100

    [#63873] Convert test_urls to pytest

--- src/os2datascanner/engine2/tests/test_urls.py
+++ src/os2datascanner/engine2/tests/test_urls.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from os2datascanner.engine2.commands.utils import DemoSourceUtility as TestSourceUtility
 from os2datascanner.engine2.model.data import DataSource
@@ -8,94 +8,91 @@ from os2datascanner.engine2.model.smb import SMBSource
 from os2datascanner.engine2.model.smbc import SMBCSource
 
 
-class URLTests(unittest.TestCase):
-    def test_sources(self):
-        sources_and_urls = [
-            (FilesystemSource("/usr"), "file:///usr"),
-            (
-                SMBSource("//10.0.0.30/Share$/Documents"),
-                "smb://10.0.0.30/Share%24/Documents",
+class TestURL:
+    @pytest.mark.parametrize("source,url", [
+        (FilesystemSource("/usr"), "file:///usr"),
+        (
+            SMBSource("//10.0.0.30/Share$/Documents"),
+            "smb://10.0.0.30/Share%24/Documents",
+        ),
+        (
+            SMBSource("//10.0.0.30/Share$/Documents", "FaithfullA"),
+            "smb://FaithfullA@10.0.0.30/Share%24/Documents",
+        ),
+        (
+            SMBSource(
+                "//10.0.0.30/Share$/Documents",
+                "FaithfullA",
+                "secretpassword",
             ),
-            (
-                SMBSource("//10.0.0.30/Share$/Documents", "FaithfullA"),
-                "smb://FaithfullA@10.0.0.30/Share%24/Documents",
+            "smb://FaithfullA:secretpassword@10.0.0.30/Share%24/Documents",
+        ),
+        (
+            SMBSource(
+                "//10.0.0.30/Share$/Documents",
+                "FaithfullA",
+                "secretpassword",
+                "SYSGRP",
             ),
-            (
-                SMBSource(
-                    "//10.0.0.30/Share$/Documents",
-                    "FaithfullA",
-                    "secretpassword",
-                ),
-                "smb://FaithfullA:secretpassword@10.0.0.30/Share%24/Documents",
+            "smb://SYSGRP;FaithfullA:secretpassword@10.0.0.30/Share%24"
+            "/Documents",
+        ),
+        (
+            SMBSource(
+                "//10.0.0.30/Share$/Documents",
+                "FaithfullA",
+                None,
+                "SYSGRP",
             ),
-            (
-                SMBSource(
-                    "//10.0.0.30/Share$/Documents",
-                    "FaithfullA",
-                    "secretpassword",
-                    "SYSGRP",
-                ),
-                "smb://SYSGRP;FaithfullA:secretpassword@10.0.0.30/Share%24"
-                "/Documents",
+            "smb://SYSGRP;FaithfullA@10.0.0.30/Share%24/Documents",
+        ),
+        (
+            SMBCSource(
+                "//INT-SRV-01/Q$",
+                "FaithfullA",
+                None,
+                "SYSGRP",
             ),
-            (
-                SMBSource(
-                    "//10.0.0.30/Share$/Documents",
-                    "FaithfullA",
-                    None,
-                    "SYSGRP",
-                ),
-                "smb://SYSGRP;FaithfullA@10.0.0.30/Share%24/Documents",
+            "smbc://SYSGRP;FaithfullA@INT-SRV-01/Q%24",
+        ),
+        (
+            SMBCSource(
+                "//INT-SRV-01.intern.vstkom.dk/Q$",
+                "FaithfullA",
+                "secretpassword",
+                None,
             ),
-            (
-                SMBCSource(
-                    "//INT-SRV-01/Q$",
-                    "FaithfullA",
-                    None,
-                    "SYSGRP",
-                ),
-                "smbc://SYSGRP;FaithfullA@INT-SRV-01/Q%24",
+            "smbc://intern.vstkom.dk;FaithfullA:secretpassword@INT-SRV-01.intern.vstkom.dk/Q%24",  # noqa
+        ),
+        (
+            SMBCSource(
+                "//INT-SRV-01.intern.vstkom.dk/Q$",
+                "FaithfullA",
+                "secretpassword",
+                "SYSGRP",
             ),
-            (
-                SMBCSource(
-                    "//INT-SRV-01.intern.vstkom.dk/Q$",
-                    "FaithfullA",
-                    "secretpassword",
-                    None,
-                ),
-                "smbc://intern.vstkom.dk;FaithfullA:secretpassword@INT-SRV-01.intern.vstkom.dk/Q%24",  # noqa
+            "smbc://SYSGRP;FaithfullA:secretpassword@INT-SRV-01.intern.vstkom.dk/Q%24",
+        ),
+        (
+            SMBCSource(
+                "//INT-SRV-01.intern.vstkom.dk/Q$",
+                "FaithfullA",
+                None,
+                "",
             ),
-            (
-                SMBCSource(
-                    "//INT-SRV-01.intern.vstkom.dk/Q$",
-                    "FaithfullA",
-                    "secretpassword",
-                    "SYSGRP",
-                ),
-                "smbc://SYSGRP;FaithfullA:secretpassword@INT-SRV-01.intern.vstkom.dk/Q%24",
-            ),
-            (
-                SMBCSource(
-                    "//INT-SRV-01.intern.vstkom.dk/Q$",
-                    "FaithfullA",
-                    None,
-                    "",
-                ),
-                "smbc://FaithfullA@INT-SRV-01.intern.vstkom.dk/Q%24",
-            ),
-            # Remove trailing / on base url if present. / should be part of the path
-            (WebSource("http://www.example.com/"), "http://www.example.com"),
-            (
-                SecureWebSource("https://www.example.com"),
-                "https://www.example.com",
-            ),
-            (
-                DataSource(b"This is a test", "text/plain"),
-                "data:text/plain;base64,VGhpcyBpcyBhIHRlc3Q=",
-            ),
-        ]
-
-        for source, url in sources_and_urls:
-            with self.subTest(url):
-                generated_url = TestSourceUtility.to_url(source)
-                self.assertEqual(url, generated_url)
+            "smbc://FaithfullA@INT-SRV-01.intern.vstkom.dk/Q%24",
+        ),
+        # Remove trailing / on base url if present. / should be part of the path
+        (WebSource("http://www.example.com/"), "http://www.example.com"),
+        (
+            SecureWebSource("https://www.example.com"),
+            "https://www.example.com",
+        ),
+        (
+            DataSource(b"This is a test", "text/plain"),
+            "data:text/plain;base64,VGhpcyBpcyBhIHRlc3Q=",
+        ),
+    ])
+    def test_sources(self, source, url):
+        generated_url = TestSourceUtility.to_url(source)
+        assert url == generated_url
