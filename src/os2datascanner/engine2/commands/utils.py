03565abebcbe1ca175a22fc0eaff709fbe57af03
Alexander Faithfull-Murawski <af@magenta.dk>
2025-03-05 11:22:01 +0100

    [#64146] Add support for SBSYS URLs to the common URL parsing function
    This commit also rewrites the function to use pattern matching, because
    the result is much nicer here

--- src/os2datascanner/engine2/commands/utils.py
+++ src/os2datascanner/engine2/commands/utils.py
@@ -27,45 +27,66 @@ class DemoSourceUtility:
 
         Supported Types:
         - SMBCSource
+          smbc://[[domain;]user[:password]@]hostname/path/to/share
         - SMBSource
+          as above
         - DropboxSource
+          dropbox://api_token
         - DataSource
+          data:mime/type[;base64],content
         - FilesystemSource
+          file:///local/filesystem/path
         - WebSource
+          http[s]://[user[:password]@]hostname/path/to/subpage
+        - SBSYS
+          sbsys://[user:password@]hostname[:port]/database_name
         '''
 
-        scheme, _, _, _, _ = urlsplit(url)
+        match urlsplit(url):
+            case ("smb" | "smbc" as scheme, netloc, path, _, _):
+                mtc = SMBSource.netloc_regex.match(netloc)
+                if mtc:
+                    if scheme == "smb":
+                        return SMBSource("//" + mtc.group("unc") + unquote(path),
+                                         mtc.group("username"), mtc.group("password"),
+                                         mtc.group("domain"))
+                    if scheme == "smbc":
+                        return SMBCSource("//" + mtc.group("unc") + unquote(path),
+                                          mtc.group("username"), mtc.group("password"),
+                                          mtc.group("domain"))
 
-        if scheme in ("smbc", "smb"):
-            _, netloc, path, _, _ = urlsplit(url.replace("\\", "/"))
-            mtc = SMBSource.netloc_regex.match(netloc)
-            if mtc:
-                if scheme == "smb":
-                    return SMBSource("//" + mtc.group("unc") + unquote(path),
-                                     mtc.group("username"), mtc.group("password"),
-                                     mtc.group("domain"))
-                if scheme == "smbc":
-                    return SMBCSource("//" + mtc.group("unc") + unquote(path),
-                                      mtc.group("username"), mtc.group("password"),
-                                      mtc.group("domain"))
+            case ("dropbox", token, _, _, _):
+                return DropboxSource(token=token)
 
-        elif scheme == "dropbox":
-            _, token, _, _, _ = urlsplit(url)
-            return DropboxSource(token=token)
+            case ("data", _, _, _, _):
+                mime, content = unpack_data_url(url)
+                return DataSource(content=content, mime=mime)
 
-        elif scheme == "data":
-            mime, content = unpack_data_url(url)
-            return DataSource(content=content, mime=mime)
+            case ("file", netloc, path, _, _):
+                assert not netloc
+                return FilesystemSource(unquote(path) if path else None)
 
-        elif scheme == "file":
-            _, netloc, path, _, _ = urlsplit(url)
-            assert not netloc
-            return FilesystemSource(unquote(path) if path else None)
+            case ("http" | "https", _, _, _, _):
+                return WebSource(url, extended_hints=True)
 
-        elif scheme in ("http", "https"):
-            return WebSource(url, extended_hints=True)
+            case ("sbsys", server, database, _, _):
+                from os2datascanner.engine2.model._staging import sbsysdb
 
-        raise UnknownSchemeError()
+                user = password = None
+                if "@" in server:
+                    auth, server = server.split("@", maxsplit=1)
+                    user, password = auth.split(":", maxsplit=1)
+
+                port = 1433
+                if ":" in server:
+                    server, port, = server.split(":", maxsplit=1)
+                    port = int(port)
+
+                return sbsysdb.SBSYSDBSource(
+                        server, port, database.lstrip("/"), user, password)
+
+            case _:
+                raise UnknownSchemeError
 
     @staticmethod
     def to_url(src) -> str:
