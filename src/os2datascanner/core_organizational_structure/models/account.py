2165fe0210e39d758bc47c142950b5bd36d72479
jdk <jdk@magenta-aps.dk>
2025-06-06 09:03:29 +0200

    [#54891] Display manager on account page in admin and report modules

--- src/os2datascanner/core_organizational_structure/models/account.py
+++ src/os2datascanner/core_organizational_structure/models/account.py
@@ -77,7 +77,7 @@ class Account(models.Model):
     )
     manager = models.ForeignKey(
         'account',
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name="managed_accounts",
