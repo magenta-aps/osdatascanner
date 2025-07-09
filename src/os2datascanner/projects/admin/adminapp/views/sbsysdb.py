7aa72f57a9d927c75ebf62f51bcc3107917d950f
Alexander Faithfull-Murawski <af@magenta.dk>
2025-07-09 09:36:30 +0200

    [#64960] Actually populate the fields when you try to copy a SBSYS DB scan

--- src/os2datascanner/projects/admin/adminapp/views/sbsysdb.py
+++ src/os2datascanner/projects/admin/adminapp/views/sbsysdb.py
@@ -1,7 +1,9 @@
 from django.views.generic import CreateView, UpdateView
+from django.utils.translation import gettext_lazy as _
 
 from ..forms.sbsysdb import SBSYSDBScannerForm
 from ..models.scannerjobs.sbsysdb import SBSYSDBScanner
+from ..models.scannerjobs.scanner import Scanner
 from .scanner_views import ScannerList, ScannerViewType
 
 
@@ -29,5 +31,18 @@ class SBSYSDBScannerUpdateDF(_Form_Mixin, UpdateView):
 class SBSYSDBScannerCopyDF(_Form_Mixin, CreateView):
     scanner_view_type = ScannerViewType.COPY
 
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return self.render_to_response(self.get_context_data())
+
     def get_initial(self):
-        return super().get_initial()
+        new_name = self.get_object().name
+        while Scanner.objects.unfiltered().filter(name=new_name).exists():
+            new_name += " " + _("Copy")
+
+        return super().get_initial() | {
+            # Copied scannerjobs should be "Invalid" by default
+            # to avoid being able to misuse this feature.
+            "validation_status": Scanner.INVALID,
+            "name": new_name
+        }
