334a43feeb62c73061484d06b473af73f5e30abe
Asbjørn Lind <asl@magenta-aps.dk>
2025-11-05 14:04:39 +0100

    [#67276] Update unit tests

--- src/os2datascanner/engine2/tests/test_external_rules.py
+++ src/os2datascanner/engine2/tests/test_external_rules.py
@@ -1,3 +1,4 @@
+import pytest
 from ..rules.external import (
     ExternallyExecutedRegexRule,
     ExternallyExecutedWordlistRule,
@@ -5,44 +6,46 @@ from ..rules.external import (
 )
 
 
-def test_ee_regex(requests_mock):
+@pytest.fixture
+def external_regex_rule():
+    return ExternallyExecutedRegexRule(
+        expression="test",
+        endpoint='http://fake-endpoint.test/predict',
+        censor_token="<censored>",
+        confidence_cutoff=0.40,
+    )
+
+
+@pytest.fixture
+def external_wordlist_rule():
+    return ExternallyExecutedWordlistRule(
+        "en_20211018_unit_test_words",
+        endpoint='http://fake-endpoint.test/predict',
+        censor_token="<sundhedsterm>",
+        confidence_cutoff=0.40,
+    )
+
+
+def test_ee_regex(requests_mock, external_regex_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_regex_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedRegexRule(expression="test", endpoint=url, censor_token="<censored>")
-
     # Act
-    result = list(rule.match("This is a test"))
+    result = list(external_regex_rule.match("This is a test"))
 
     # Assert
     assert len(result) == 1
     assert result[0]['context'] == "This is a <censored>"
 
 
-def test_ee_regex_confidentely_wrong(requests_mock):
+def test_ee_regex_insecure(requests_mock, external_regex_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
-    requests_mock.get(url, json={"prediction": 0, "confidence": 1.00})
-
-    rule = ExternallyExecutedRegexRule(expression="test", endpoint=url, censor_token="<censored>")
-
-    # Act
-    result = list(rule.match("This is a test"))
-
-    # Assert
-    assert len(result) == 0
-
-
-def test_ee_regex_insecure(requests_mock):
-    # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_regex_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 0.15})
 
-    rule = ExternallyExecutedRegexRule(expression="test", endpoint=url, censor_token="<censored>")
-
     # Act
-    result = list(rule.match("This is a test"))
+    result = list(external_regex_rule.match("This is a test"))
 
     # Assert
     assert len(result) == 0
@@ -53,7 +56,12 @@ def test_ee_regex_no_match(requests_mock):
     url = 'http://fake-endpoint.test/predict'
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedRegexRule(expression="exam", endpoint=url, censor_token="<censored>")
+    rule = ExternallyExecutedRegexRule(
+        expression="exam",
+        endpoint=url,
+        censor_token="<censored>",
+        confidence_cutoff=0.40
+    )
 
     # Act
     result = list(rule.match("This is a test"))
@@ -62,107 +70,63 @@ def test_ee_regex_no_match(requests_mock):
     assert len(result) == 0
 
 
-def test_ee_regex_censor_multiple(requests_mock):
+def test_ee_regex_censor_multiple(requests_mock, external_regex_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_regex_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedRegexRule(expression="test", endpoint=url, censor_token="<censored>")
-
     # Act
-    result = list(rule.match("testing, one two three, testing"))
+    result = list(external_regex_rule.match("testing, one two three, testing"))
 
     # Assert
     assert len(result) == 1
     assert result[0]['context'] == "<censored>ing, one two three, <censored>ing"
 
 
-def test_ee_wordlist(requests_mock):
+def test_ee_wordlist(requests_mock, external_wordlist_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_wordlist_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedWordlistRule(
-        "en_20211018_unit_test_words",
-        endpoint=url,
-        censor_token="<sundhedsterm>",
-    )
-
     # Act
-    result = list(rule.match("Kol er noget farligt noget"))
+    result = list(external_wordlist_rule.match("Kol er noget farligt noget"))
 
     # Assert
     assert len(result) == 1
     assert result[0]['context'] == "<sundhedsterm> er noget farligt noget"
 
 
-def test_ee_wordlist_confidentely_incorrect(requests_mock):
+def test_ee_wordlist_insecure(requests_mock, external_wordlist_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
-    requests_mock.get(url, json={"prediction": 0, "confidence": 1.00})
-
-    rule = ExternallyExecutedWordlistRule(
-        "en_20211018_unit_test_words",
-        endpoint=url,
-        censor_token="<sundhedsterm>",
-    )
-
-    # Act
-    result = list(rule.match("Kol er noget farligt noget"))
-
-    # Assert
-    assert len(result) == 0
-
-
-def test_ee_wordlist_insecure(requests_mock):
-    # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_wordlist_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 0.15})
 
-    rule = ExternallyExecutedWordlistRule(
-        "en_20211018_unit_test_words",
-        endpoint=url,
-        censor_token="<sundhedsterm>",
-    )
-
     # Act
-    result = list(rule.match("Kol er noget farligt noget"))
+    result = list(external_wordlist_rule.match("Kol er noget farligt noget"))
 
     # Assert
     assert len(result) == 0
 
 
-def test_ee_wordlist_no_match(requests_mock):
+def test_ee_wordlist_no_match(requests_mock, external_wordlist_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_wordlist_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedWordlistRule(
-        "en_20211018_unit_test_words",
-        endpoint=url,
-        censor_token="<sundhedsterm>",
-    )
-
     # Act
-    result = list(rule.match("Ko er noget farligt noget"))
+    result = list(external_wordlist_rule.match("Ko er noget farligt noget"))
 
     # Assert
     assert len(result) == 0
 
 
-def test_ee_wordlist_censor_multiple(requests_mock):
+def test_ee_wordlist_censor_multiple(requests_mock, external_wordlist_rule):
     # Arrange
-    url = 'http://fake-endpoint.test/predict'
+    url = external_wordlist_rule.endpoint
     requests_mock.get(url, json={"prediction": 1, "confidence": 1.00})
 
-    rule = ExternallyExecutedWordlistRule(
-        "en_20211018_unit_test_words",
-        endpoint=url,
-        censor_token="<sundhedsterm>",
-    )
-
     # Act
-    result = list(rule.match("Jeg har både kol og pest"))
+    result = list(external_wordlist_rule.match("Jeg har både kol og pest"))
 
     # Assert
     assert len(result) == 1
