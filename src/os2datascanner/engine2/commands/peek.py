9f51285b1a13f0fc4148af11704f69e4fba91b88
Alexander Faithfull-Murawski <af@magenta.dk>
2025-12-02 11:30:12 +0100

    [#55702] Flesh out the peek command a bit more
    Why yes, I have been using this to test ContentSkippedMessages, why do
    you ask

--- src/os2datascanner/engine2/commands/peek.py
+++ src/os2datascanner/engine2/commands/peek.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import json
+import click
 
 from os2datascanner.engine2.pipeline import messages
 from os2datascanner.engine2.pipeline.exporter import censor_outgoing_message
-from os2datascanner.engine2.pipeline.utilities.pika import PikaPipelineThread
+from os2datascanner.engine2.pipeline.utilities import pika
 
 
 class_mapping = {
@@ -20,7 +22,18 @@ class_mapping = {
 }
 
 
-class Printer(PikaPipelineThread):
+class Printer(pika.PikaPipelineThread):
+    def __init__(
+            self, *args, reject, censor, pretty_print, timeout, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reject = reject
+        self.censor = censor
+        self.pretty_print = pretty_print
+        self.timeout = timeout
+
+    def render_json(self, body):
+        return json.dumps(body, indent=self.pretty_print or None)
+
     def handle_message(self, routing_key, body):
         if routing_key == "os2ds_results":
             routing_key = body["origin"]
@@ -33,27 +46,62 @@ class Printer(PikaPipelineThread):
             pass
 
         if not message:
-            print(
-                    "warning: unrecognised message format, printing uncensored"
-                    " content", file=sys.stderr)
-            print(body)
+            if self.censor:
+                print(
+                        "warning: unrecognised message format,"
+                        " printing uncensored content", file=sys.stderr)
+            print(self.render_json(body))
         else:
-            print(
-                    "information: message format recognised, printing censored"
-                    " content", file=sys.stderr)
-            print(censor_outgoing_message(message).to_json_object())
+            if self.censor:
+                print(
+                        "information: message format recognised, printing censored"
+                        " content", file=sys.stderr)
+                pm = censor_outgoing_message(message)
+            else:
+                pm = message
+            print(self.render_json(pm.to_json_object()))
+
+        if self.reject:
+            raise pika.RejectMessage(requeue=False)
 
         # Enqueueing a stop command before handle_message returns ensures that
         # we'll disconnect before we acknowledge recept of this message
         self.enqueue_stop()
         yield from []
 
+    def after_timeout(self):
+        print("Timed out, stopping", file=sys.stderr)
+        self.enqueue_stop()
+        self.join()
 
-def main():
-    for queue in sys.argv[1:]:
-        print(f"Waiting for a message on queue {queue}...")
-        with Printer(read={queue}) as p:
-            p.run_consumer()
+    def after_message(self, routing_key, body, *, ex=None):
+        # Presumably we got here because we rejected the message, so let's
+        # just stop
+        self.enqueue_stop()
+
+
+@click.command()
+@click.option("--reject/--no-reject", "reject",
+              default=False,
+              is_flag=True, help="reject (discard) the displayed message")
+@click.option("--censor/--no-censor", "censor",
+              default=True,
+              is_flag=True, help="censor the message before displaying it")
+@click.option("--pretty-print/--ugly-print", "pretty_print",
+              default=True,
+              is_flag=True, help="print the message with indentation")
+@click.option('--timeout', type=float, default=30.0,
+              help='wait for a message for the given number of seconds')
+@click.argument("queues", nargs=-1)
+def main(
+        reject: bool, censor: bool, pretty_print: bool, timeout: float,
+        queues: tuple[str, ...]):
+    print(f"Waiting for a message on any of {queues}...")
+    with Printer(
+            read=set(queues),
+            reject=reject, censor=censor, pretty_print=pretty_print,
+            timeout=timeout) as p:
+        p.run_consumer()
 
 
 if __name__ == '__main__':
