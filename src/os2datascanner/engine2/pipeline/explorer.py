bce19fdfd6d63a82fe15952848570830287da7ef
Alexander Faithfull-Murawski <af@magenta.dk>
2026-02-02 15:08:01 +0100

    [#44442] Add a variant form of the explorer stage with no serialisation

--- src/os2datascanner/engine2/pipeline/explorer.py
+++ src/os2datascanner/engine2/pipeline/explorer.py
@@ -1,4 +1,7 @@
+from collections.abc import Generator
 import structlog
+
+from os2datascanner.engine2.model.core.utilities import SourceManager
 from ..model.core import (
         Source, takes_named_arg, UnknownSchemeError, DeserialisationError)
 from ..model.core.errors import (ModelException,
@@ -27,68 +30,44 @@ def process_exploration_error(scan_spec, handle_candidate, ex):
     exception_message = (
             f"Exploration error. {type(ex).__name__}: "
             + ", ".join(str(a) for a in ex.args))
-    problem_message = messages.ProblemMessage(
+    yield messages.ProblemMessage(
             scan_tag=scan_spec.scan_tag, source=scan_spec.source,
             handle=handle_candidate, message=exception_message)
-    # We send problem messages to os2ds_problems to create or update a
-    # DocumentReport about the problem, and to os2ds_checkups to create a
-    # UserErrorLog object.
-    for problem_queue in ("os2ds_problems", "os2ds_checkups"):
-        yield (problem_queue, problem_message.to_json_object())
     logger.info(
             "found problem",
             scan_tag=scan_spec.scan_tag, handle=handle_candidate)
 
 
-def message_received_raw(body, channel, source_manager):  # noqa
-    try:
-        scan_tag = messages.ScanTagFragment.from_json_object(body["scan_tag"])
-    except KeyError:
-        # Scan specifications with no scan tag are simply invalid and should be
-        # dropped
-        return
-
-    try:
-        scan_spec = messages.ScanSpecMessage.from_json_object(body)
-
-        if scan_spec.progress:
-            progress = scan_spec.progress
-            scan_spec = scan_spec._replace(progress=None)
-        else:
-            progress = messages.ProgressFragment(
-                    rule=scan_spec.rule, matches=[])
-    except UnknownSchemeError as ex:
-        yield ("os2ds_problems", messages.ProblemMessage(
-                scan_tag=scan_tag, source=None, handle=None,
-                message=("Unknown scheme '{0}'".format(
-                        ex.args[0]))).to_json_object())
-        return
-    except (KeyError, DeserialisationError):
-        yield ("os2ds_problems", messages.ProblemMessage(
-                scan_tag=scan_tag, source=None, handle=None,
-                message="Malformed input").to_json_object())
-        return
-
+def message_received(  # noqa: CCR001
+        message: messages.ScanSpecMessage,
+        sm: SourceManager) -> Generator[messages.SerialisableMessage]:
     error_count = 0
     handle_count = 0
     source_count = None
     exception_message = ""
 
+    if message.progress:
+        progress = message.progress
+        message = message._replace(progress=None)
+    else:
+        progress = messages.ProgressFragment(
+                rule=message.rule, matches=[])
+
     # Update the configuration of the source manager.
     # Yes, this is dreaded mutable state... Just don't go change it
     # somewhere else.
-    source_manager.configuration = scan_spec.configuration
+    sm.configuration = message.configuration
 
-    handles_method = scan_spec.source.handles
+    handles_method = message.source.handles
 
     # Inspect the handles() method to see if it can take any extra hints
     extra_kwargs = {}
     if takes_named_arg(handles_method, "rule"):
         extra_kwargs["rule"] = progress.rule
 
-    handle_iterator = scan_spec.source.handles(source_manager, **extra_kwargs)
+    handle_iterator = message.source.handles(sm, **extra_kwargs)
 
-    log = logger.bind(scan_tag=scan_tag)
+    log = logger.bind(scan_tag=message.scan_tag)
 
     try:
         for handle in handle_iterator:
@@ -96,24 +75,21 @@ def message_received_raw(body, channel, source_manager):  # noqa
                 # We were able to construct a Handle for something that
                 # exists, but then something unexpected (that we can tie to
                 # that specific Handle) went wrong. Send a problem message
-                yield from process_exploration_error(scan_spec, *handle)
+                yield from process_exploration_error(message, *handle)
                 error_count += 1
-            elif not scan_spec.source.yields_independent_sources:
+            elif not message.source.yields_independent_sources:
                 # This Handle is just a normal reference to a scannable object.
                 # Send it on to be processed
 
-                yield (scan_spec.conversion_queue,
-                       messages.ConversionMessage(
-                            scan_spec, handle, progress).to_json_object())
+                yield messages.ConversionMessage(message, handle, progress)
                 handle_count += 1
             else:
                 # Check if the handle should be excluded.
-                if is_handle_relevant(handle, scan_spec.filter_rule):
+                if is_handle_relevant(handle, message.filter_rule):
                     # This Handle is a thin wrapper around an independent Source.
                     # Construct that Source and enqueue it for further exploration
                     new_source = Source.from_handle(handle)
-                    yield (scan_spec.explorer_queue, scan_spec._replace(
-                        source=new_source).to_json_object())
+                    yield message._replace(source=new_source)
                     source_count = (source_count or 0) + 1
                 else:
                     log.info("handle excluded", handle=handle)
@@ -138,10 +114,9 @@ def message_received_raw(body, channel, source_manager):  # noqa
             exception_message = "Exploration error. {0}: ".format(type(e).__name__)
             exception_message += ", ".join([str(a) for a in e.args])
 
-        problem_message = messages.ProblemMessage(
-            scan_tag=scan_tag, source=scan_spec.source, handle=None,
-            message=exception_message)
-        yield ("os2ds_problems", problem_message.to_json_object())
+        yield messages.ProblemMessage(
+                scan_tag=message.scan_tag, source=message.source, handle=None,
+                message=exception_message)
         log.warning(
                 "finished unsuccessfully",
                 handle_count=handle_count, source_count=source_count,
@@ -151,11 +126,53 @@ def message_received_raw(body, channel, source_manager):  # noqa
         # good form to clean up
         if hasattr(handle_iterator, "close"):
             handle_iterator.close()
-        yield ("os2ds_status", messages.StatusMessage(
-                scan_tag=scan_tag,
+        yield messages.StatusMessage(
+                scan_tag=message.scan_tag,
                 total_objects=handle_count, new_sources=source_count,
                 message=exception_message,
-                status_is_error=exception_message != "").to_json_object())
+                status_is_error=exception_message != "")
+
+
+def message_received_raw(body, channel, source_manager):
+    try:
+        scan_tag = messages.ScanTagFragment.from_json_object(body["scan_tag"])
+    except KeyError:
+        # Scan specifications with no scan tag are simply invalid and should be
+        # dropped
+        return
+
+    try:
+        scan_spec = messages.ScanSpecMessage.from_json_object(body)
+    except UnknownSchemeError as ex:
+        yield ("os2ds_problems", messages.ProblemMessage(
+                scan_tag=scan_tag, source=None, handle=None,
+                message=("Unknown scheme '{0}'".format(
+                        ex.args[0]))).to_json_object())
+        return
+    except (KeyError, DeserialisationError):
+        yield ("os2ds_problems", messages.ProblemMessage(
+                scan_tag=scan_tag, source=None, handle=None,
+                message="Malformed input").to_json_object())
+        return
+
+    for m in message_received(scan_spec, source_manager):
+        queues: list[str]
+        if isinstance(m, messages.StatusMessage):
+            queues = ["os2ds_status"]
+        elif isinstance(m, messages.ScanSpecMessage):
+            queues = [scan_spec.explorer_queue]
+        elif isinstance(m, messages.ConversionMessage):
+            queues = [scan_spec.conversion_queue]
+        elif isinstance(m, messages.ProblemMessage):
+            # We send problem messages to os2ds_problems to create or update a
+            # DocumentReport about the problem, and to os2ds_checkups to create a
+            # UserErrorLog object.
+            queues = ["os2ds_problems", "os2ds_checkups"]
+        else:
+            raise TypeError(type(m))
+        json_form = m.to_json_object()
+        for q in queues:
+            yield (q, json_form)
 
 
 if __name__ == "__main__":
