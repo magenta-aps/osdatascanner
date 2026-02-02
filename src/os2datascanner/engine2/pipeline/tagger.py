168e518518247e87375d7530ae1159a5a5828c10
Alexander Faithfull-Murawski <af@magenta.dk>
2026-02-02 14:55:35 +0100

    [#44442] Add a variant form of the tagger stage with no serialisation

--- src/os2datascanner/engine2/pipeline/tagger.py
+++ src/os2datascanner/engine2/pipeline/tagger.py
@@ -1,30 +1,46 @@
+from collections.abc import Generator
+
+from os2datascanner.engine2.model.core.utilities import SourceManager
 from .. import settings
 from ..utilities.backoff import TimeoutRetrier
 from . import messages
 
 
-def message_received_raw(body, channel, source_manager):
-    message = messages.HandleMessage.from_json_object(body)
-
+def message_received(
+        message: messages.HandleMessage,
+        sm: SourceManager) -> Generator[messages.SerialisableMessage]:
     tr = TimeoutRetrier(
             seconds=settings.pipeline["op_timeout"],
             max_tries=settings.pipeline["op_tries"])
 
     try:
-        resource = message.handle.follow(source_manager)
+        resource = message.handle.follow(sm)
         metadata = tr.run(resource.get_metadata)
-        yield ("os2ds_metadata",
-               messages.MetadataMessage(
-                        message.scan_tag, message.handle, metadata
-                ).to_json_object())
+        yield messages.MetadataMessage(
+                message.scan_tag, message.handle, metadata)
     except Exception as e:
         exception_message = (
             "Metadata extraction error. {0}: ".format(type(e).__name__))
         exception_message += ", ".join([str(a) for a in e.args])
-        yield ("os2ds_problems", messages.ProblemMessage(
+        yield messages.ProblemMessage(
                 scan_tag=message.scan_tag,
                 source=None, handle=message.handle,
-                message=exception_message).to_json_object())
+                message=exception_message)
+
+
+def message_received_raw(body, channel, source_manager):
+    for m in message_received(
+            messages.HandleMessage.from_json_object(body),
+            source_manager):
+        queue: str | None
+        if isinstance(m, messages.MetadataMessage):
+            queue = "os2ds_metadata"
+        elif isinstance(m, messages.ProblemMessage):
+            queue = "os2ds_problems"
+        else:
+            raise TypeError(type(m))
+        if queue:
+            yield (queue, m.to_json_object())
 
 
 if __name__ == "__main__":
