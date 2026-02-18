4efa35f23a38ec34409c32361d00ebe3ae2ce2bf
sabrinaos <sos@magenta-aps.dk>
2026-02-18 13:36:50 +0100

    [#68847] Add is_archive to the result view context.

--- src/os2datascanner/projects/report/reportapp/views/report_views.py
+++ src/os2datascanner/projects/report/reportapp/views/report_views.py
@@ -150,6 +150,9 @@ class ReportView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        context["is_archive"] = self.archive
+
         context["renderable_rules"] = RENDERABLE_RULES
 
         # We do this at the request of a customer to change the structure of resolution choices.
