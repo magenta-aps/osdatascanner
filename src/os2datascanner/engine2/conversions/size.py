953a9ed33e2639f439e82aa98fb289e9cb787c35
Asbjørn Lind <asl@magenta-aps.dk>
2026-02-19 14:43:51 +0100

    [#68865] Restrict SizeRule to pdfs

--- src/os2datascanner/engine2/conversions/size.py
+++ src/os2datascanner/engine2/conversions/size.py
@@ -7,7 +7,7 @@ from .types import OutputType
 from .registry import conversion
 
 
-@conversion(OutputType.Size)
+@conversion(OutputType.Size, "application/pdf")
 def size_processor(resource):
     if hasattr(resource, "get_size"):
         return resource.get_size()
