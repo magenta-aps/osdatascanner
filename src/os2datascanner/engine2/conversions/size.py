ce190c36b003550550e69825649e75d4e3fb6763
Asbjørn Lind <asl@magenta-aps.dk>
2026-02-19 11:58:32 +0100

    [#68865] Add SizeRule

---
+++ src/os2datascanner/engine2/conversions/size.py
@@ -0,0 +1,15 @@
+# Part of the OSdatascanner system, copyright © 2014-2026 Magenta ApS.
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, you can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+from .types import OutputType
+from .registry import conversion
+
+
+@conversion(OutputType.Size)
+def size_processor(resource):
+    if hasattr(resource, "get_size"):
+        return resource.get_size()
+    else:
+        return None
--- src/os2datascanner/engine2/rules/meta.py
+++ src/os2datascanner/engine2/rules/meta.py
@@ -5,6 +5,7 @@
 
 from ..conversions.types import OutputType
 from .rule import Rule, SimpleRule
+from .utilities.properties import RuleProperties, RulePrecedence
 
 
 @Rule.register_class
@@ -42,4 +43,46 @@ class HasConversionRule(SimpleRule):
     def _get_constructor_kwargs(cls, obj):
         return super()._get_constructor_kwargs(obj) | {
             "target": OutputType(obj["target"]),
+        }
+
+
+@Rule.register_class
+class SizeRule(SimpleRule):
+    operates_on = OutputType.Size
+    type_label = "size"
+    properties = RuleProperties(
+        precedence=RulePrecedence.LEFT,
+        standalone=True)
+
+    def __init__(self, size, synthetic=True, **super_kwargs):
+        super().__init__(synthetic=synthetic, **super_kwargs)
+        self._size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def presentation_raw(self):
+        return "size bigger than {0}".format(self.size)
+
+    def match(self, content):
+        if content is None:
+            return
+
+        if content > self.size:
+            yield {
+                "match": content
+            }
+
+    def to_json_object(self):
+        return dict(
+            **super().to_json_object(),
+            size=self.size,
+        )
+
+    @classmethod
+    def _get_constructor_kwargs(cls, obj):
+        return super()._get_constructor_kwargs(obj) | {
+            "size": obj["size"],
         }
