d3acbc42d061b723836bb64d0dbfeb769c491c9d
Alexander Faithfull-Murawski <af@magenta.dk>
2026-03-16 13:41:30 +0100

    [#43190] Add a rotating developer name carousel to the credits page

--- src/os2datascanner/projects/shared/egg.py
+++ src/os2datascanner/projects/shared/egg.py
@@ -44,3 +44,150 @@ register_converter(EggConverter, "egg")
 
 class EggView(LoginRequiredMixin, TemplateView):
     template_name = "components/credits/credits.html"
+
+    def get_context_data(self, **kwargs):
+        # Don't call up to TemplateView/ContextMixin.get_context_data -- the
+        # credits list is the only thing that should be available
+        return {
+            "repetitions": [0, 1],
+            "everybody": [
+                {
+                    "name": "Alexander Faithfull-Murawski",
+                },
+                {
+                    "name": "Alexander Normann Jepsen",
+                },
+                {
+                    "name": "Anders Jepsen",
+                },
+                {
+                    "name": "Andreas Brodersen",
+                },
+                {
+                    "name": "Andreas Kring",
+                },
+                {
+                    "name": "Andreas Poulsen",
+                },
+                {
+                    "name": "Andreas Natanel Nielsen",
+                },
+                {
+                    "name": "Asbjørn Lind",
+                },
+                {
+                    "name": "Carl Bordum Hansen",
+                },
+                {
+                    "name": "Carsten Agger",
+                },
+                {
+                    "name": "Casper V. Kristensen",
+                },
+                {
+                    "name": "Clara Vejlø Schlemmer",
+                },
+                {
+                    "name": "Dan Villiom Podlaski Christiansen",
+                },
+                {
+                    "name": "Danni Als",
+                },
+                {
+                    "name": "Emil Graae Norsker",
+                },
+                {
+                    "name": "Emil Nissen",
+                },
+                {
+                    "name": "Emil Thorenfeldt",
+                },
+                {
+                    "name": "Emil Witt Hansen",
+                },
+                {
+                    "name": "Fleming Heide Pedersen",
+                },
+                {
+                    "name": "Frank Thomsen",
+                },
+                {
+                    "name": "Io Boye Pinnerup",
+                },
+                {
+                    "name": "Jakob Rydhof",
+                },
+                {
+                    "name": "Jesper Dam Knudgaard",
+                },
+                {
+                    "name": "Jonas Kofoed Hansen",
+                },
+                {
+                    "name": "Jørgen Gårdsted Jørgensen",
+                },
+                {
+                    "name": "Jørgen Ulrik B. Krag",
+                },
+                {
+                    "name": "Laura Grønborg Sørensen",
+                },
+                {
+                    "name": "Marcus Funch",
+                },
+                {
+                    "name": "Mathias Dannesbo",
+                },
+                {
+                    "name": "Mikkel Aleister Clausen",
+                },
+                {
+                    "name": "Mikkel Rostved",
+                },
+                {
+                    "name": "Miklas Bøgvald",
+                },
+                {
+                    "name": "Nikolaj Sievertsen Nørring",
+                },
+                {
+                    "name": "Novik Singh",
+                },
+                {
+                    "name": "Oliver Eierstrand",
+                },
+                {
+                    "name": "Paw Møller",
+                },
+                {
+                    "name": "Petter Becker-Jostes",
+                },
+                {
+                    "name": "Rasmus Kristian Koefoed",
+                },
+                {
+                    "name": "Robert Jensen",
+                },
+                {
+                    "name": "Sabrina Sørensen",
+                },
+                {
+                    "name": "Seth Yastrov",
+                },
+                {
+                    "name": "Steffen Jørgensen",
+                },
+                {
+                    "name": "Stine Nyhus",
+                },
+                {
+                    "name": "Toke Fritzemeier",
+                },
+                {
+                    "name": "Tomas Hagenau Andersen",
+                },
+                {
+                    "name": "Youssef El-Mannouti",
+                }
+            ]
+        }
